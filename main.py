0fddf1025ff8236a9702c736b924ff3ea6c487e1
Roseco-crs <rodolphe.segbedji.aims.ac.rw>
2024-01-20 10:21:52 +0100

    defined a function to chunk a text using Langchain

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_text_chunks(text):
         separator = "\n",
         chunk_size = 1000,
         chunk_overlap = 200,
-        length_function = len
+        length_function = len,
     )
     chunks = text_splitter.split_text(text)
     return chunks
