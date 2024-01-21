1f1370151596546244c346e88e212d734695d4f8
Roseco-crs <rodolphe.segbedji.aims.ac.rw>
2024-01-21 14:47:53 +0100

    automatic prompt generation

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def main():
     user_question = st.text_input("Ask a question about your documents")
     if user_question:
         handle_userinput(user_question)
+        
 
     with st.sidebar :
         st.subheader("Your documents")
